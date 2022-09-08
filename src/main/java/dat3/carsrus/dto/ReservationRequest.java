163e209416e2f888b5421cfe8628465c6dbd5314
Lasse Rasmussen <lass531n@stud.kea.dk>
2022-09-08 13:08:02 +0200

    Reservation controller, request og response tilføjet. commit til docker demo

---
+++ src/main/java/dat3/carsrus/dto/ReservationRequest.java
@@ -0,0 +1,18 @@
+package dat3.carsrus.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationRequest {
+
+    String username;
+    int carId;
+    String localDate;
+
+}
